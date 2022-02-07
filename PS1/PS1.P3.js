e8e9a636ca87685cc0c7eefed512b5f738bb0a34
Tin Nguyen <tinhoang@bu.edu>
2022-02-07 16:52:16 -0500

    comments on problem 3

--- PS1/PS1.P3.js
+++ PS1/PS1.P3.js
@@ -5,11 +5,13 @@ const apply = (string, fun) => {
     return (fun(string))
 }
 
+// Expression One
 const exprOne = 'supercalifragilisticexpialidocious';
 console.log(`${apply(exprOne, str => str.replaceAll('c', '?c').split('?'))}`)
 
+// Expression Two
 const exprTwo = 'supercalifragilisticexpialidocious';
-let res = apply(exprOne, str => (
+let res = apply(exprTwo, str => (
     {
         originalString: str,
         modifiedString: str.replaceAll('a', 'A'),
